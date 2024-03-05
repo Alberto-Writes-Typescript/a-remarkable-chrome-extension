646935283fd91f8131c966414cfb6abe0f5492ae
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-03-05 08:46:15 +0100

    Refactor esbuild configuration for better readability

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,53 +1,60 @@
-import dotenv from 'dotenv';
-import esbuild from 'esbuild';
+import path from 'path'
+import dotenv from 'dotenv'
+import esbuild from 'esbuild'
+
+const BUILD_CONFIG = {
+	bundle: true,
+	// Path to application.js folder
+	absWorkingDir: path.join(process.cwd(), 'src'),
+	// Application.js file, used by Rails to bundle all JS Rails code
+	entryPoints: ['index.ts'],
+	// Destination of JS bundle, points to the Rails JS Asset folder
+	outdir: path.join(process.cwd(), 'public/build/bundle.js'),
+	// Split option is disabled, only needed when using multiple input files
+	// More information: https://esbuild.github.io/api/#splitting (change it if using multiple inputs)
+	splitting: false,
+	// Remove unused JS methods
+	treeShaking: true,
+	// Adds mapping information so web browser console can map bundle errors to the corresponding
+	// code line and column in the real code
+	// More information: https://esbuild.github.io/api/#sourcemap
+	sourcemap: process.argv.includes('--development'),
+	// Compresses bundle
+	// More information: https://esbuild.github.io/api/#minify
+	minify: process.argv.includes('--production'),
+	// Removes all console lines from bundle
+	// More information: https://esbuild.github.io/api/#drop
+	drop: process.argv.includes('--production') ? ['console'] : [],
+	// Build command log output: https://esbuild.github.io/api/#log-level
+	logLevel: 'info'
+}
 
 dotenv.config();
 
-const args = process.argv;
+// Script to just bundle project JS
+if (process.argv.includes('--production')) {
+	esbuild.build(BUILD_CONFIG)
 
-const config = {
-	logLevel: 'info',
-	entryPoints: ['src/index.ts'],
-	outfile: 'public/build/bundle.js',
-	bundle: true,
-	define: {
-		NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
-	},
-};
-
-if (args.includes('--build')) {
+// Script to bundle JS and re-bundle when files change
+} else {
 	esbuild
-		.build({
-			...config,
-			minify: true,
-			sourcemap: false,
-		})
-		.catch((e) => {
-			console.error(e);
-			process.exit(1);
-		});
-}
-
-if (args.includes('--start')) {
-	esbuild
-		.context({
-			...config,
-			minify: false,
-			sourcemap: true,
-		})
+		.context(BUILD_CONFIG)
 		.then(async (ctx) => {
-			// this is needed only if live reloading will be used
 			await ctx.watch();
 
 			await ctx.serve({
-				servedir: 'public',
+				// Public directory served by ESBuild server
+				servedir: path.join(process.cwd(), 'public'),
+				// ESBuild Server Log - Displays request information
 				onRequest: ({ remoteAddress, method, path, status, timeInMS }) => {
 					console.info(remoteAddress, status, `"${method} ${path}" [${timeInMS}ms]`);
 				},
-			});
+			})
 		})
+		// Display bundling errors in console
 		.catch((e) => {
 			console.error(e);
 			process.exit(1);
-		});
+		})
 }
+
