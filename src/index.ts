3498ad8d60f39abd19bec0e55c7fb20abe4e63ed
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-03-05 11:10:13 +0100

    chore: setup ESLint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import { createElement } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from "./App.js";
+import { createElement } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App.js'
 
-const rootElem = document.getElementById('root');
+const rootElem = document.getElementById('root')
 
-if (rootElem)
+if (rootElem) {
   createRoot(rootElem).render(
     createElement(
       App,
       { message: 'Hello World! A Counter App built on ESBuild + React + Typescript' },
       null
     )
-  );
-else alert('Cannot find element with id "root", something went wrong');
-
+  )
+} else alert('Cannot find element with id "root", something went wrong')
